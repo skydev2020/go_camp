f2db731d8cc734f532e456118e80da9ecad28f4f
skydev2020 <skydev2020@outlook.com>
2020-12-15 00:03:26 -0500

    randomizing question

--- main.go
+++ main.go
@@ -24,8 +24,24 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 	questions := loadRecords(*csvFile)
+	correct :=0
+	total :=len(questions)
+
+	if *randomize {
+		questions = shuffle(questions)
+	}
 }
 
 func loadRecords(csvFile string) [][]string {
+	content, err := ioutil.ReadFile(csvFile)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	r := csv.NewReader(bytes.NewReader(content))
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return records
 }
