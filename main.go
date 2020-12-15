5ae7d27bb7624d2eeb23cde2596693e8c11cdd3c
skydev2020 <skydev2020@outlook.com>
2020-12-14 22:57:25 -0500

    func csv file loading

--- main.go
+++ main.go
@@ -15,5 +15,17 @@ import (
 )
 
 func main() {
+	csvFile := flag.String("csv", "problems.csv", "csv file with questions and answers")
+	duration := flag.Int("time", 30, "quiz time limit")
+	randomize := flag.Bool("random", false, "randomize questions")
+
+	//Parse
+	flag.Parse()
+
+	reader := bufio.NewReader(os.Stdin)
+	questions := loadRecords(*csvFile)
+}
+
+func loadRecords(csvFile string) [][]string {
 
 }
